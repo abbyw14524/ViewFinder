da6c13582eaf40f33b45374214c89135804b9cb2
Apple <apple@Apples-MacBook-Pro-6.local>
2019-06-12 09:38:06 -0700

    IterationOne

--- ViewFinder/AddPhotoViewController.swift
+++ ViewFinder/AddPhotoViewController.swift
@@ -14,19 +14,30 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
+    @IBAction func cameraButton(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    @IBAction func photoLibraryButton(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+         present(imagePicker, animated: true, completion: nil)
+    }
+    @IBAction func saveButton(_ sender: Any) {
+        
+    }
+    @IBAction func albumButton(_ sender: Any) {
+        
+    }
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let selectedImage = info [UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = selectedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
     }
-    */
-
 }
